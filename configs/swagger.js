786b415bdd33eeb208fdc56fa62c428b8f3c3977
JodFimu <josedavidfimu@gmail.com>
2025-02-24 08:27:46 -0600

    add: se añadio la base del proyecto

---
+++ configs/swagger.js
@@ -0,0 +1,29 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options ={
+    swaggerDefinition:{
+        openapi:"3.0.0",
+        info:{
+            title: "Tienda Online API",
+            version: "1.0.0",
+            description: "API para un sistema de gestion de empresas",
+            contact:{
+                name: "José Figueroa",
+                email: "jfigueroa-2023015@kinal.edu.gt"
+            }
+        },
+        servers:[
+            {
+                url: "http://127.0.0.1:3000/coperex/v1"
+            }
+        ]
+    },
+    apis:[
+        "./src/auth/auth.routes.js"
+    ]
+}
+
+const swaggerDocs = swaggerJSDoc(options)
+
+export { swaggerDocs, swaggerUi}
