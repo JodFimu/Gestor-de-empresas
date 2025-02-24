9afb2c8e81d70f5e401542463afcd9b0b84e1d1e
JodFimu <josedavidfimu@gmail.com>
2025-02-24 10:53:34 -0600

    User module (#1)
    * feat: funciones de autorizacion agregadas
    
    * feat: se agrego la funcionalidad de actualizar la contraseña

--- configs/server.js
+++ configs/server.js
@@ -7,6 +7,8 @@ import morgan from "morgan";
 import { dbConnection } from "./mongo.js";
 import { swaggerDocs, swaggerUi } from "./swagger.js";
 import  apiLimiter from "../src/middlewares/rate-limit-validator.js";
+import authRoutes from "../src/auth/auth.routes.js"
+import userRoutes from "../src/user/user.routes.js"
 
 
 const middlewares = (app) => {
@@ -20,6 +22,8 @@ const middlewares = (app) => {
 
 const routes = (app) => {
     app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+    app.use("/interferCoperex/v1/auth", authRoutes);
+    app.use("/interferCoperex/v1/user", userRoutes);
 }
 
 const conectarDB = async () => {
@@ -37,7 +41,6 @@ export const initServer = () => {
         middlewares(app);
         conectarDB();
         routes(app);
-        //createAdmin();
         const port = process.env.PORT; 
         app.listen(port, () => {
             console.log(`Server running on port ${port}`);
