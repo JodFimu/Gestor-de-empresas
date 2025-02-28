39c8ceecc66de48d593216119233e59c12e2131a
JodFimu <josedavidfimu@gmail.com>
2025-02-28 10:50:19 -0600

    feat: se agrego la funcionalidad para generar reportes por filtro

--- src/enterprise/enterprise.routes.js
+++ src/enterprise/enterprise.routes.js
@@ -1,11 +1,12 @@
 import {Router} from "express";
 
-import { createEnterprise, updateEnterprise} from "./enterprise.controller.js";
+import { createEnterprise, updateEnterprise, generateReport } from "./enterprise.controller.js";
 import { addEnterpriseValidators , updateEnterpriseValidators} from "../middlewares/enterprise-validators.js"
 
 const router = Router();
 
 router.post("/addEnterprise", addEnterpriseValidators, createEnterprise);
 router.put("/updateEnterprise/:eid", updateEnterpriseValidators, updateEnterprise);
+router.get("/getEnterprises", generateReport);
 
 export default router;
